aa04e593b7d471f40066828cf9f18d1ac877d517
Will Figueiredo <will@wfigueiredo.com>
2018-11-21 18:03:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,14 @@ class Song
   end
 
   def self.create
-    new_song=self.new
+    new_song = self.new
+    new_song.save
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new
+    new_song.name=(name)
     new_song.save
     new_song
   end
