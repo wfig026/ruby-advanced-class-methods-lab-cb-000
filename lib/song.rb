9fe2999dc599ef0966396f3a3bfa82dd8f624fee
Will Figueiredo <will@wfigueiredo.com>
2018-11-21 18:14:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,14 @@ class Song
   end
 
   def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   end
 
   def self.destroy_all
